d750cc17a3c96ee720f219cc735729a4677bf991
ganezasan <boss@cheekit.com>
2016-10-11 18:41:49 +0900

    fix two grid layout

--- asset/partybingo.js
+++ asset/partybingo.js
@@ -1,121 +1,114 @@
-(function(){
-	var pingoNumber = $('#pingo-number');
-	var startButton = $('#start-button');
-	var resetButton = $('#reset-button');
-	var historiesDiv = $('#histories')
-	var drumAudio = $('#drum').get(0);
-	
-	// init histories
-	var toBingoString = function(n){
-		if(n > 9) {
-			return n.toString(10);
-		} else if (n < 0) {	
-			return '00'
-		} else {
-			return '0' +  n.toString(10);
-		}
-	}
-	var addHistory = function(n) {
-		historiesDiv.append('<div class="col-md-1"><p class="history-number">' + toBingoString(n) + '</p></div>')
-	};
-	
-	// init number list and storage
-	var numberListAll = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75];
-	var storage = localStorage;
-	var listKey = 'partybingo.numberlist';
-	var removedKey = 'partybingo.removedlist';
-	var setNumberList = function(a) {
-		storage.setItem(listKey, JSON.stringify(a));
-	};
-	var getNumberList = function() {
-		return JSON.parse(storage.getItem(listKey));
-	};
-	var setRemovedList = function(a) {
-		storage.setItem(removedKey, JSON.stringify(a));
-	};
-	var getRemovedList = function() {
-		return JSON.parse(storage.getItem(removedKey));
-	};
-	var resetLists = function() {
-		setNumberList(numberListAll.concat());
-		setRemovedList([]);
-	};
-	
-	// create initial list or loadHistory
-	var loadedNumberList = getNumberList();
-	var loadedRemovedList = getRemovedList();
-	if(loadedNumberList && loadedRemovedList) {
-		for (var i = 0; i < loadedRemovedList.length; i++) {
-			addHistory(loadedRemovedList[i]);
-		}
-	} else {
-		resetLists();
-	} 
+"use strict";
 
-	// create util method
-	var getNumberRamdom = function(){
-		var numberList = getNumberList();
-		var i = Math.floor(Math.random() * numberList.length);
-		return numberList[i];
+const pingoNumber = $('#pingo-number');
+const startButton = $('#start-button');
+const resetButton = $('#reset-button');
+const historiesDiv = $('#histories');
+const drumAudio = $('#drum').get(0);
+
+// init histories
+const toBingoString = (n) => {
+	if(n > 9) {
+		return n.toString(10);
+	} else if (n < 0) {
+		return '00';
+	} else {
+		return '0' +  n.toString(10);
 	}
-	var removeNumberRamdom = function(){
-		var numberList = getNumberList();
-		if(numberList.length === 0) {
-			return -1;
-		}
-		var i = Math.floor(Math.random() * numberList.length);
-		var removed = numberList[i];
-		numberList.splice(i, 1);
-		setNumberList(numberList);
-		var removedList = getRemovedList();
-		removedList.push(removed);
-		setRemovedList(removedList)
-		return removed;
+};
+
+const addHistory = (n) => {
+	historiesDiv.append('<span class="history-number">' + toBingoString(n) + '</span>');
+};
+
+// init number list and storage
+const numberListAll = Array.from(Array(75).keys()).map((i) => i+1);
+const storage = localStorage;
+const listKey = 'partybingo.numberlist';
+const removedKey = 'partybingo.removedlist';
+const setNumberList = (a) => storage.setItem(listKey, JSON.stringify(a));
+const getNumberList = () => JSON.parse(storage.getItem(listKey));
+const setRemovedList = (a) =>	storage.setItem(removedKey, JSON.stringify(a));
+const getRemovedList = () => JSON.parse(storage.getItem(removedKey));
+const resetLists = () => {
+	setNumberList(numberListAll.concat());
+	setRemovedList([]);
+};
+
+// create initial list or loadHistory
+const loadedNumberList = getNumberList();
+const loadedRemovedList = getRemovedList();
+if(loadedNumberList && loadedRemovedList) {
+	for (var i = 0; i < loadedRemovedList.length; i++) {
+		addHistory(loadedRemovedList[i]);
 	}
-	
-	// init start button
-	var isStarted = false;
-	function rourletto() {
-		if(isStarted) {
-			pingoNumber.text(toBingoString(getNumberRamdom()));
-			setTimeout(rourletto, 60);
-		}
-	} 
-	var stop = function(time) {
-		isStarted = false;
-		startButton.text('Start');
-		var n = removeNumberRamdom();
-		pingoNumber.text(toBingoString(n));
-		addHistory(n);
+} else {
+	resetLists();
+}
+
+// create util method
+const getNumberRamdom = () => {
+	const numberList = getNumberList();
+	const i = Math.floor(Math.random() * numberList.length);
+	return numberList[i];
+};
+
+const removeNumberRamdom = () => {
+	const numberList = getNumberList();
+	if(numberList.length === 0) {
+		return -1;
+	}
+	const i = Math.floor(Math.random() * numberList.length);
+	const removed = numberList[i];
+	numberList.splice(i, 1);
+	setNumberList(numberList);
+	const removedList = getRemovedList();
+	removedList.push(removed);
+	setRemovedList(removedList);
+	return removed;
+};
+
+// init start button
+let isStarted = false;
+function rourletto() {
+	if(isStarted) {
+		pingoNumber.text(toBingoString(getNumberRamdom()));
+		setTimeout(rourletto, 60);
+	}
+}
+const stop = () => {
+	isStarted = false;
+	startButton.text('Start');
+	const n = removeNumberRamdom();
+	pingoNumber.text(toBingoString(n));
+	addHistory(n);
+	drumAudio.pause();
+};
+const start = () => {
+	isStarted = true;
+	startButton.text('Stop');
+	drumAudio.currentTime = 0;
+	drumAudio.play();
+	rourletto();
+};
+const startClicked = () => {
+	if(isStarted) {
+		stop(null);
+	} else {
+		start();
+	}
+};
+startButton.click(startClicked); // button
+startButton.focus();
+
+// init reset button
+const resetClicked = () => {
+	if (window.confirm('Do you really want to reset?')) {
+		resetLists();
+		pingoNumber.text('00');
+		historiesDiv.empty();
 		drumAudio.pause();
-	};
-	var start = function(){
-		isStarted = true;
-		startButton.text('Stop');
-		drumAudio.currentTime = 0; 
-		drumAudio.play();
-		rourletto();
-	};
-	var startClicked = function(e){
-		if(isStarted) {
-			stop(null);
-		} else {
-			start();
-		}
-	};
-	startButton.click(startClicked); // button
-	startButton.focus();
-	
-	// init reset button
-	var resetClicked = function() {
-		if (confirm('Do you really want to reset?')) {
-			resetLists();
-			pingoNumber.text('00');
-			historiesDiv.empty();
-			drumAudio.pause();
-			startButton.focus();
-		}
-	};
-	resetButton.click(resetClicked);
-	
-})()
+		startButton.focus();
+	}
+};
+resetButton.click(resetClicked);
